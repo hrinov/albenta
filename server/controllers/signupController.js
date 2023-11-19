7101c8a01731d902a5337bbbcd5be5690bbef659
Ruslan Hrinov <ruslan.hrinov@bndigital.co>
2023-11-19 17:16:09 +0200

    configure data sending

--- server/controllers/signupController.js
+++ server/controllers/signupController.js
@@ -1,8 +1,33 @@
 const { v4: uuidv4 } = require('uuid');
+const bcrypt = require('bcrypt')
+require('dotenv').config()
+const jwt = require('jsonwebtoken');
 
 const addUser = (req, res) => {
+    let { email, password } = req.body
 
-    return res.status(200).json({ "message": "success" })
+    if (!email || !password) {
+        return res.status(400).json({ "message": "email and password are required" })
+    }
+
+    function generateAccessToken(payload) {
+        return jwt.sign(payload, process.env.TOKEN_SECRET, { expiresIn: '15m' });
+    }
+
+    function generateRefreshToken() {
+        return uuidv4();
+    }
+
+    const data = {
+        email: email,
+        password: bcrypt.hashSync(password, 10),
+        accessToken: generateAccessToken({ email }),
+        refreshToken: generateRefreshToken()
+    }
+
+    console.log(data)
+
+    return res.status(200).json({ "success": true })
 }
 
 
--- website/src/pages/SignUpPage/index.tsx
+++ website/src/pages/SignUpPage/index.tsx
@@ -1,15 +1,25 @@
 const url = import.meta.env.VITE_URL;
-import { FC } from "react";
+import { FC, useState } from "react";
 import "./index.sass";
 const SignupPage: FC = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
   const registerUser = async () => {
-    const response = await fetch(`${url}/api/register`, {
+    const data = {
+      email: email,
+      password: password,
+    };
+    let response = await fetch(`${url}/api/register`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      // body: JSON.stringify(userData),
+      body: JSON.stringify(data),
     });
+
+    response = await response.json();
+    console.log(response);
   };
 
   const handleClick = () => {
@@ -20,8 +30,16 @@ const SignupPage: FC = () => {
     <section className="signup-page">
       <div className="inputs-wrapper">
         <h1>Sign Up</h1>
-        <input placeholder="email" />
-        <input placeholder="password" />
+        <input
+          value={email}
+          placeholder="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          value={password}
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <div className="ok-btn" onClick={handleClick}>
           NEXT
         </div>
