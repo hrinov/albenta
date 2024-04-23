8482f0ebe011863da60a1539b08f9c8e87730db7
Ruslan Hrinov <139871255+hrinov@users.noreply.github.com>
2024-04-23 11:13:28 +0300

    Update skeleton rules

--- website/src/pages/Account/pages/Plans/index.tsx
+++ website/src/pages/Account/pages/Plans/index.tsx
@@ -132,7 +132,7 @@ const Plans: FC = () => {
           </div>
           <Doughnut
             data={charts[i]}
-            className={`${deposits ? "" : "skeleton"}`}
+            className={`${deposits ? "" : "transparent"}`}
           />
           <button
             disabled={!!time || !deposits}
@@ -145,6 +145,7 @@ const Plans: FC = () => {
             src={loadingAnimation}
             style={{ opacity: loading == i ? 0.8 : 0 }}
           />
+          <div className={`skeleton-circle ${deposits ? "hide" : ""}`} />
           <div className={`white-circle ${deposits ? "" : "hide"}`} />
           {time && <div className="timer">{time}h</div>}
         </div>
